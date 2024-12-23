27a9c587fb2cd175eb67b09360004d4ad1e828f7
vikas <vikasreddy270@gmail.com>
2024-12-23 18:00:31 -0500

    working changes added

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// Add this JavaScript code
+document.querySelector('.experience-details-container').addEventListener('wheel', function(e) {
+  if (e.deltaY !== 0) {
+      e.preventDefault(); // Prevent vertical scrolling
+      this.scrollLeft += e.deltaY; // Scroll horizontally instead
+  }
+}, { passive: false }); // passive: false is required to use preventDefault()
